4e8730219f083f5016724076d117e690ff8c8f99
Christopher Magana <cmagana@geisel-software-com>
2025-06-06 12:58:23 -0400

    added ip support for multiple operating systems

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,24 @@
 # This is where aliases are for oh my zsh
-alias ip="ipconfig getifaddr en0"
+
+# OS detection for platform-specific commands
+case "$OSTYPE" in
+  darwin*)
+    # macOS specific aliases
+    alias getip="ipconfig getifaddr en0"
+    ;;
+  linux*)
+    # Linux specific aliases
+    alias getip="hostname -I | awk '{print \$1}'"
+    ;;
+  msys*|cygwin*|mingw*)
+    # Windows specific aliases
+    alias getip="ipconfig | grep IPv4 | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | head -1"
+    ;;
+  *)
+    # Fallback for other systems
+    alias getip="echo 'IP command not configured for this OS'"
+    ;;
+esac
 
 # config shortcuts
 alias zconf="vim ~/.zshrc"
